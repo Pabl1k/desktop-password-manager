777704bc7e0953550b33032805ef5614c6f85d92
Pabl1k <poiumazaya@gmail.com>
2025-02-23 20:15:40 +0200

    project setup (electron + react)

---
+++ src/electron/main.ts
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev, PORT } from "./utils.js";
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+
+  if (isDev()) {
+    mainWindow.loadURL(`http://localhost:${PORT}`);
+  } else {
+    mainWindow.loadURL(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
+});

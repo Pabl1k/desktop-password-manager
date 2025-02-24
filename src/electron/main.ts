390bb370ef09a027b7cb0d063da8a5386c0317e6
Pabl1k <poiumazaya@gmail.com>
2025-02-24 19:03:04 +0200

    app startup changes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,14 @@ import path from 'path';
 import { isDev, PORT } from './utils.js';
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    autoHideMenuBar: true
+  });
+
+  mainWindow.maximize();
 
   if (isDev()) {
     mainWindow.loadURL(`http://localhost:${PORT}`);

db88aa0ef23f82a6c65edd9c6b80ab4a221951f4
Pabl1k <poiumazaya@gmail.com>
2025-03-12 00:18:22 +0200

    password generator modal

--- src/client/common/utils.ts
+++ src/client/common/utils.ts
@@ -1,2 +1,26 @@
 export const uniqueId = (length = 16) =>
   [...Array(length)].map(() => Math.random().toString(36).charAt(2)).join('');
+
+export const generatePassword = (length = 16) => {
+  const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const symbols = '!@#$%^&*()_+{}[]<>?/~|';
+
+  const allChars = lowercase + uppercase + numbers + symbols;
+  let password = '';
+
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  for (let i = 4; i < length; i++) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  return password
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
+};
---
+++ src/client/components/passwordGenerator/PasswordGeneratorModal.tsx
@@ -0,0 +1,73 @@
+import { FC, useState } from 'react';
+import clsx from 'clsx';
+import Button from '../../common/components/button/Button';
+import Modal from '../../common/components/modal/Modal';
+import { generatePassword } from '../../common/utils';
+import styles from './PasswordGeneratorModal.module.scss';
+
+interface ApplyButton {
+  text: string;
+  onClick: (password: string) => void;
+}
+
+interface Props {
+  open: boolean;
+  applyButton?: ApplyButton;
+  onClose: () => void;
+}
+
+const PasswordGeneratorModal: FC<Props> = ({ open, applyButton, onClose }) => {
+  const [value, setValue] = useState(generatePassword());
+  const [copied, setCopied] = useState(false);
+
+  const getApplyButtonText = () => {
+    if (applyButton) {
+      return applyButton.text;
+    }
+
+    return copied ? 'Copied to clipboard' : 'Copy to clipboard';
+  };
+
+  const handleRegenerate = () => {
+    setValue(generatePassword());
+    setCopied(false);
+  };
+
+  const handleApplyButtonClick = async () => {
+    if (applyButton) {
+      applyButton.onClick(value);
+      setValue(generatePassword());
+      return;
+    }
+
+    await navigator.clipboard.writeText(value);
+    setCopied(true);
+  };
+
+  const handleClose = () => {
+    setCopied(false);
+    onClose();
+  };
+
+  return (
+    <Modal open={open} className="p-6 flex flex-col justify-between h-[300px] max-w-[400px]">
+      <span className="text-2xl flex justify-center">Generate password</span>
+      <div>
+        <div className={clsx(styles.passwordField, 'pl-2 py-2 text-3xl')}>{value}</div>
+        <div className="flex justify-center mt-2">
+          <Button onClick={handleRegenerate}>Regenerate</Button>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button type="add" disabled={copied} onClick={handleApplyButtonClick}>
+          {getApplyButtonText()}
+        </Button>
+        <Button className="ml-2" onClick={handleClose}>
+          Cancel
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default PasswordGeneratorModal;

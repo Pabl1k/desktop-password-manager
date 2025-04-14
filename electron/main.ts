5b67e5bce83d7442dad694ccba93cf7d541306b2
Pabl1k <poiumazaya@gmail.com>
2025-04-14 11:29:29 +0300

    app icon setup

--- electron/main.ts
+++ electron/main.ts
@@ -1,15 +1,58 @@
 import { app, BrowserWindow, shell } from 'electron';
+import os from 'os';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const devMode = process.env.NODE_ENV === 'development';
 const PORT = 1234;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getCurrentOS = () => {
+  const platform = os.platform();
+
+  if (platform === 'win32') {
+    return 'windows';
+  }
+  if (platform === 'darwin') {
+    return 'macos';
+  }
+  if (platform === 'linux') {
+    return 'linux';
+  }
+
+  return 'unknown';
+};
+
+const getIconExtensionByPlatform = () => {
+  const platform = getCurrentOS();
+
+  if (platform === 'macos') {
+    return 'icns';
+  }
+  if (platform === 'linux') {
+    return 'png';
+  }
+
+  return 'ico';
+};
+
+const getIconPath = () => {
+  const iconExt = getIconExtensionByPlatform();
+
+  return devMode
+    ? path.join(__dirname, `../assets/icons/icon.${iconExt}`)
+    : path.join(process.resourcesPath, 'dist-icons', `icon.${iconExt}`);
+};
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     frame: true,
-    autoHideMenuBar: true
+    autoHideMenuBar: true,
+    icon: getIconPath()
   });
 
   mainWindow.on('ready-to-show', () => mainWindow.show());

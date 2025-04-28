907498c717385f15b9bd4918126f880f1d8fed8d
Pabl1k <poiumazaya@gmail.com>
2025-04-28 15:00:19 +0300

    0.1.10

--- src/containers/Sidebar.tsx
+++ src/containers/Sidebar.tsx
@@ -57,7 +57,7 @@ const Sidebar: FC<Props> = ({ setView }) => {
           </Button>
         ))}
       </div>
-      Auto update test: 28.04 - 13:09
+      Auto update test: 28.04 - 15:00
       <div className="flex justify-center">
         <span className="text-sm">{`${t('version')}: ${APP_VERSION}`}</span>
       </div>

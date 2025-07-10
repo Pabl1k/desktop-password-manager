d03c184a258930e942b9369baf8d3c627a78f5c8
Pavel <pavel.zaplatkin@horiondigital.com>
2025-07-10 22:53:20 +0300

    TS checker setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import viteCompression from 'vite-plugin-compression';
 import svgr from 'vite-plugin-svgr';
 import { version } from './package.json';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(), viteCompression()],
+  plugins: [react(), tailwindcss(), svgr(), viteCompression(), checker({ typescript: true })],
   base: './',
   resolve: {
     alias: {
